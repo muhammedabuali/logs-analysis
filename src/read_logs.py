d47e5ce3891e9560e82ef8e384573d2ec9a9ee0b
Mohamed H. A. Ibrahim <muhammedabuali@gmail.com>
2025-02-12 04:02:04 +0100

    parse log msg for driving and starting an order

--- src/read_logs.py
+++ src/read_logs.py
@@ -13,6 +13,12 @@ def process_line(line):
     added_schedule = []
     if process_schedule_message(added_schedule, line):
         return
+    starting_events = []
+    if process_starting_event_message(starting_events, line):
+        return
+    driving_events = []
+    if process_driving_event_message(driving_events, line):
+        return
 
 def process_added_container_message(added_containers, line):
     pattern = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) INFO adding TO (TO_CO_\w+), .+'
@@ -24,13 +30,33 @@ def process_added_container_message(added_containers, line):
     return False
 
 
-def process_schedule_message(added_containers, line):
+def process_schedule_message(added_schedule, line):
     pattern = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) INFO (\w+) schedule (.+)'
     matching = re.match(pattern, line)
     if matching:
         timestamp, vehicle_id, schedule = matching.groups()
-        added_containers.append([timestamp, vehicle_id, schedule])
-        print(f"timestamp {timestamp} added for vehicle {vehicle_id} schedule {schedule}")
+        added_schedule.append([timestamp, vehicle_id, schedule])
+        # print(f"timestamp {timestamp} added for vehicle {vehicle_id} schedule {schedule}")
+        return True
+    return False
+
+def process_starting_event_message(recorded_events, line):
+    pattern = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) INFO (SC\w+) starting (.+)'
+    matching = re.match(pattern, line)
+    if matching:
+        timestamp, vehicle_id, event = matching.groups()
+        recorded_events.append([timestamp, vehicle_id, event])
+        # print(f"timestamp {timestamp} event for vehicle {vehicle_id} : {event}")
+        return True
+    return False
+
+def process_driving_event_message(recorded_events, line):
+    pattern = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) INFO (\w+) (\(.+\)) driving to (.+); (.+) s; (.+) mm'
+    matching = re.match(pattern, line)
+    if matching:
+        timestamp, vehicle_id, body, loc_id, seconds, distance = matching.groups()
+        recorded_events.append([timestamp, vehicle_id, body, loc_id, seconds, distance])
+        # print(f"timestamp {timestamp} driving vehicle {vehicle_id} to location {loc_id} for time {seconds} s")
         return True
     return False
 
